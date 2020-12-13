019f88fcffea8b0fbbe2c6b345fc18b5302285bd
Bogdan <stefpop25@gmail.com>
2020-12-13 16:00:00 +0200

    Tabel

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <html>
     <head>
         <title>AntiLampa</title>
+        <link rel="stylesheet" href="css/main.css" />
     </head>
     <body>
+        <table>
+            <tr>
+                <h1 class="titlu">Dinosaurii lu'Lica</h1>
+            </tr>
+        </table>
     </body>
 </html>
