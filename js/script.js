e45462cbee0d44b61acebd046fb825a31146bab6
IonutSima <simaandreiionut2@gmail.com>
2020-12-15 16:03:31 +0200

    Mici modificari

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 window.onscroll = function(){
     var Scroll = window.pageYOffset;
     if(Scroll > 250){
-        document.getElementById("menu").style.top = "-250px";
+        document.getElementById("menu").style.top = "-200px";
     }
     else{
         document.getElementById("menu").style.top = "0px";
