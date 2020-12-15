d78f722cbef7d0db24cafb7a7ce05b4d19bf0e3d
IonutSima <simaandreiionut2@gmail.com>
2020-12-15 15:33:32 +0200

    Am mai lucrat la about page

--- pagini/about.php
+++ pagini/about.php
@@ -5,11 +5,13 @@
     <a href="#p1"><img src="poze/Vulturu'.jpeg" alt=""></a>
     <a href="#p2"><img src="poze/IonutCap-de-prost.jpg" alt=""></a>
     <a href="#p3"><img src="poze/Razvan-pula-mea.jpg" alt=""></a>
+    <a href="#p4"><img src="poze/4481011c-4378-4b82-b4eb-2f40703eb0ce.png" alt=""></a>
+    <a href="#p5"><img src="poze/monkas.png" alt=""></a>
 
  </div>
 
 <div class="section" id="p1">
-    <span class="name">Bogdan Viorel</span>
+<span class="name">Bogdan Viorel</span>
     <span class="border"></span>
     <p> 
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ratione voluptates unde
@@ -30,6 +32,26 @@
 
 <div class="section" id="p3">
     <span class="name">Razvan Barosan</span>
+    <span class="border"></span>
+    <p> 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ratione voluptates unde
+         quas pariatur labore earum tempora dolorum, sequi, culpa commodi at ducimus mollitia
+          quis asperiores laboriosam enim eius rerum.
+    </p>
+</div>
+
+<div class="section" id="p4">
+    <span class="name">Vlad VTM</span>
+    <span class="border"></span>
+    <p> 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ratione voluptates unde
+         quas pariatur labore earum tempora dolorum, sequi, culpa commodi at ducimus mollitia
+          quis asperiores laboriosam enim eius rerum.
+    </p>
+</div>
+
+<div class="section" id="p5">
+    <span class="name">Stefan_Tech</span>
     <span class="border"></span>
     <p> 
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ratione voluptates unde
