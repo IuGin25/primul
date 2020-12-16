025cd52eefc096e8a67c72ece6e3d1e0f9e7666e
IlikeEndermen <vvladmlg@gmail.com>
2020-12-16 19:42:28 +0200

    Am facut shop :3

---
+++ pagini/shop.php
@@ -0,0 +1,133 @@
+<div class="main">
+    <div class="main1">
+<div class="cards">
+<div class="image">
+<img src="poze/contact-background.png">
+</div>
+<div class="title">
+<h1>Title Name</h1>
+</div>
+<div class="des">
+<p>Write your description here...</p>
+<button>Read More...</button>
+</div>
+</div>
+
+    <!--cards owo-->
+    <div class="cards">
+<div class="image1">
+<img src="poze/IonutCap-de-prost.jpg">
+</div>
+<div class="title">
+<h1>Title Name</h1>
+</div>
+<div class="des">
+<p>Write your description here...</p>
+<button>Read More...</button>
+</div>
+</div>
+
+    <!--cards owo-->
+<div class="cards">
+<div class="image2">
+<img src="poze/furry1.png">
+</div>
+<div class="title">
+<h1>Title Name</h1>
+</div>
+<div class="des">
+<p>Write your description here...</p>
+<button>Read More...</button>
+</div>
+</div>
+<!--cards owo-->
+<div class="cards">
+<div class="image1">
+<img src="poze/IonutCap-de-prost.jpg">
+</div>
+<div class="title">
+<h1>Title Name</h1>
+</div>
+<div class="des">
+<p>Write your description here...</p>
+<button>Read More...</button>
+</div>
+</div>
+
+    <!--cards owo-->
+<div class="cards">
+<div class="image2">
+<img src="poze/furry1.png">
+</div>
+<div class="title">
+<h1>Title Name</h1>
+</div>
+<div class="des">
+<p>Write your description here...</p>
+<button>Read More...</button>
+</div>
+</div>
+</div>
+
+<!--cards owo-->
+<div class="cards">
+<div class="image1">
+<img src="poze/IonutCap-de-prost.jpg">
+</div>
+<div class="title">
+<h1>Title Name</h1>
+</div>
+<div class="des">
+<p>Write your description here...</p>
+<button>Read More...</button>
+</div>
+</div>
+
+    <!--cards owo-->
+<div class="cards">
+<div class="image2">
+<img src="poze/furry1.png">
+</div>
+<div class="title">
+<h1>Title Name</h1>
+</div>
+<div class="des">
+<p>Write your description here...</p>
+<button>Read More...</button>
+</div>
+</div>
+<!--cards owo-->
+<div class="cards">
+<div class="image1">
+<img src="poze/IonutCap-de-prost.jpg">
+</div>
+<div class="title">
+<h1>Title Name</h1>
+</div>
+<div class="des">
+<p>Write your description here...</p>
+<button>Read More...</button>
+</div>
+</div>
+
+    <!--cards owo-->
+<div class="cards">
+<div class="image2">
+<img src="poze/furry1.png">
+</div>
+<div class="title">
+<h1>Title Name</h1>
+</div>
+<div class="des">
+<p>Write your description here...</p>
+<button>Read More...</button>
+</div>
+</div>
+</div>
+
+<!-- Incerc sa bag muzica pe shop acum sa nu va mirati-->
+<div class="music">
+<audio controls autoplay loop>
+<source src="muzica/Postal 2 Map Music.mp3" type="audio/ogg">
+</audio>
+</div>
